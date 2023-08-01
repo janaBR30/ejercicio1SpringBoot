6ddf15d2bedd6e10194c347ab428ec71900c8bdf
Janaina Bacin <janabacrauber@gmail.com>
2023-08-01 11:22:31 -0300

    convert gallons to liters metod

--- src/main/java/com/Ejercicio1/ejercicio1/Controller/Controller.java
+++ src/main/java/com/Ejercicio1/ejercicio1/Controller/Controller.java
@@ -1,13 +1,17 @@
 package com.Ejercicio1.ejercicio1.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController  //mapeando controlador con anotation 
 public class Controller {
     
-    @GetMapping ("/Jana")  //mapeando metodo con endpoint(path) solicitud GET
-    public String sayHello() {
-        return "Hola Jana";
+    @GetMapping ("/convertir/{galones}")  //mapeando metodo con endpoint(path) y parametro(variable) con solicitud GET
+    public String galonesALitros(@PathVariable double galones) {
+        
+        //convertimos los galones a litros
+        double litros = galones * 3.78541;
+        return "La cantidad de litros equivalente a: " + galones + " galones " + "es de " + litros + " litros";
                 }
 }
